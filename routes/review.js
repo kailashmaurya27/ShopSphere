ef5a67ea89a7cd161981b051a2daa6c51de315b3
kailashmaurya27 <mauryakailash1@gmail.com>
2024-10-14 23:16:59 +0530

    Validation Done

--- routes/review.js
+++ routes/review.js
@@ -2,17 +2,23 @@ const express = require('express');
 const Product = require('../models/Product');
 const Review = require('../models/Review');
 const router = express.Router() //mini instance
+const {validateReview} = require('../middleware');
 
-router.post('/products/:id/review', async (req, res)=>{
-    let {id} = req.params;
-    let {rating, comment} = req.body;
-    const product = await Product.findById(id);
-    const review = new Review({rating, comment});
+router.post('/products/:id/review', validateReview, async (req, res)=>{
+    try{
+        let {id} = req.params;
+        let {rating, comment} = req.body;
+        const product = await Product.findById(id);
+        const review = new Review({rating, comment});
 
-    product.reviews.push(review);
-    await review.save(); // save is a mongoDB operation so we have used await function
-    await product.save();
-    res.redirect(`/products/${id}`);
+        product.reviews.push(review);
+        await review.save(); // save is a mongoDB operation so we have used await function
+        await product.save();
+        res.redirect(`/products/${id}`);
+    }
+    catch(e){
+        res.status(500).render('error', {err: e.message});
+    }
 })
 
 
